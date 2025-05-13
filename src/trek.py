55e163b510e37502c1aeeaffd3b234303f9ecf2b
Yashwanth A <yash2706.git@gmail.com>
2025-05-13 20:54:02 +0530

    add tree like visual update

--- src/trek.py
+++ src/trek.py
@@ -11,10 +11,14 @@ unichar = {
 # Use item.name for item's name without full path
 def explore(dir, prefix=""):
     dir_path = Path(dir)
-    for item in dir_path.iterdir():
-        if item.is_file():
-            print(f"{unichar['space']}{prefix}{item.name}")
-        elif item.is_dir():
-            print(f"{unichar['space']}{prefix}{item.name}")
-            explore(item, prefix+unichar["space"])
+    entries = list(dir_path.iterdir())
+    entries.sort()
+    for index, item in enumerate(entries):
+        is_last = (index == len(entries)-1)
+        roots = unichar["lastchild"] if is_last else unichar["branch"]
+        print(f"{prefix}{roots}{item.name}")
+        
+        if item.is_dir():
+            extension = unichar["space"] if is_last else unichar["vertical_line"]
+            explore(item, prefix+extension)
             

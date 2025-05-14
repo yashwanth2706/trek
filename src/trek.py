0e153b36e45dd0e494f2bf56a5c37260d8f1b7b7
Yashwanth A <yash2706.git@gmail.com>
2025-05-14 07:27:49 +0530

    Added proper whitespacing format for output in unichar

--- src/trek.py
+++ src/trek.py
@@ -3,7 +3,7 @@ from pathlib import Path
 unichar = {
     "branch":"├──",
     "lastchild":"└──",
-    "vertical_line":"│",
+    "vertical_line":"│   ",
     "horizontal_line":"──",
     "space":"    ",
     }
@@ -11,8 +11,7 @@ unichar = {
 # Use item.name for item's name without full path
 def explore(dir, prefix=""):
     dir_path = Path(dir)
-    entries = list(dir_path.iterdir())
-    entries.sort()
+    entries = sorted(list(dir_path.iterdir()))
     for index, item in enumerate(entries):
         is_last = (index == len(entries)-1)
         roots = unichar["lastchild"] if is_last else unichar["branch"]
@@ -21,4 +20,3 @@ def explore(dir, prefix=""):
         if item.is_dir():
             extension = unichar["space"] if is_last else unichar["vertical_line"]
             explore(item, prefix+extension)
-            
